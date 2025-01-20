30b8232324df42ea11ca9946d144013552ee92c5
Vaisakh P V <vaisakhpv033@gmail.com>
2025-01-20 21:46:31 +0530

    completed features section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  )
+}
+
+export default App
